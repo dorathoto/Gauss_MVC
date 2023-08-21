739bcba394dc7b8d4ea68c003cd420a64aa61e0b
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2023-08-20 22:09:29 -0300

    final capitulo 2

--- LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/Controllers/HomeController.cs
@@ -6,19 +6,7 @@ namespace LanchesMac.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
