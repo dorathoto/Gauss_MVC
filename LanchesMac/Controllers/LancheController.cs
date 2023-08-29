a9f866f24d2247442008ebbca7dea14fa40421da
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2023-08-29 15:24:22 -0300

    Video 2. Ajustando o código da View List

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,20 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
