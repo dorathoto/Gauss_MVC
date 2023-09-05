add77e7f9425d8b9dc6660dfc5207a8a80850402
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2023-09-05 15:51:31 -0300

    aula 11 - migration carrinho

---
+++ LanchesMac/Migrations/20230905185019_CarrinhoCompraItem.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    /// <inheritdoc />
+    public partial class CarrinhoCompraItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarrinhoCompraItens",
+                columns: table => new
+                {
+                    CarrinhoCompraItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LancheId = table.Column<int>(type: "int", nullable: true),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    CarrinhoCompraId = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarrinhoCompraItens", x => x.CarrinhoCompraItemId);
+                    table.ForeignKey(
+                        name: "FK_CarrinhoCompraItens_Lanches_LancheId",
+                        column: x => x.LancheId,
+                        principalTable: "Lanches",
+                        principalColumn: "LancheId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarrinhoCompraItens_LancheId",
+                table: "CarrinhoCompraItens",
+                column: "LancheId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarrinhoCompraItens");
+        }
+    }
+}
---
+++ LanchesMac/Models/CarrinhoCompraItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
+{
+   // [Table("CarrinhoCompraItens")]
+    public class CarrinhoCompraItem
+    {
+        public int CarrinhoCompraItemId { get; set; }
+        public Lanche Lanche { get; set; }
+
+        public int Quantidade { get; set; }
+
+        [StringLength(200)]
+        public string CarrinhoCompraId { get; set; }
+    }
+}
