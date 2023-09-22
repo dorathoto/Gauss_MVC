c2c0e0472ad9ac2dda6e8a864f52d91b5eb45d1b
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2023-09-22 14:18:18 -0300

    video 5 checkout

---
+++ LanchesMac/Controllers/PedidoController.cs
@@ -0,0 +1,31 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            carrinhoCompra = _carrinhoCompra;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Checkout()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Checkout(Pedido pedido)
+        {
+            return View();
+        }
+    }
+}
