df8f7d8a3ee07d564e82e8e4f3fdc7d7e23d2234
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2023-10-20 14:23:21 -0300

    crud

---
+++ LanchesMac/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
