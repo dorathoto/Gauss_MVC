3930594dc5c7a7794b0ef2a4ed93887e9fffe132
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2023-10-21 11:43:31 -0300

    [Authorize] no carrinhocompras

--- LanchesMac/Controllers/CarrinhoCompraController.cs
+++ LanchesMac/Controllers/CarrinhoCompraController.cs
@@ -1,6 +1,7 @@
 ﻿using LanchesMac.Models;
 using LanchesMac.Repositories.Interfaces;
 using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers;
@@ -31,6 +32,7 @@ public class CarrinhoCompraController : Controller
         return View(carrinhoCompraVM);
     }
 
+    [Authorize]
 
     public IActionResult AdicionarItemNoCarrinhoCompra(int lancheId)
     {
@@ -43,6 +45,8 @@ public class CarrinhoCompraController : Controller
         }
         return RedirectToAction("Index");
     }
+
+    [Authorize]
 
     public IActionResult RemoverItemDoCarrinhoCompra(int lancheId)
     {
