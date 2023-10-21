39adea6fc9646168d5db17fdf3b6fa5e9dd043fe
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2023-10-21 10:42:32 -0300

    feat accountController, views login

---
+++ LanchesMac/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers;
+
+[Authorize]
+public class AccountController : Controller
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public AccountController(UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    [AllowAnonymous]
+    public IActionResult Login(string returnUrl)
+    {
+        return View(new LoginViewModel()
+        {
+            ReturnUrl = returnUrl
+        });
+    }
+
+    [AllowAnonymous]
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel loginVM)
+    {
+        if (!ModelState.IsValid)
+            return View(loginVM);
+
+        var user = await _userManager.FindByNameAsync(loginVM.UserName);
+
+        if (user != null)
+        {
+            var result = await _signInManager.PasswordSignInAsync(user,
+                loginVM.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                if (string.IsNullOrEmpty(loginVM.ReturnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return Redirect(loginVM.ReturnUrl);
+            }
+        }
+        ModelState.AddModelError("", "Falha ao realizar o login!!");
+        return View(loginVM);
+    }//
+
+}
